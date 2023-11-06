aca467e7f43994fef24546101c7f06b2ebd4ab14
melaimmohamedanis <76123728+melaimmohamedanis@users.noreply.github.com>
2023-11-06 10:34:45 +0100

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -14,7 +14,7 @@ const Home = () => {
   useEffect(()=>{
     setLoading(true);
     axios
-    .get('http://localhost:3000/books')
+    .get('https://new-mern-server4.vercel.app/books')
     .then((response)=>{
       setBooks(response.data.data);
       setLoading(false);
