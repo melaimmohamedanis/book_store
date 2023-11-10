b21754865318e6c842f407984bb50e59c49895ab
melaimmohamedanis <76123728+melaimmohamedanis@users.noreply.github.com>
2023-11-10 16:54:05 +0100

    Update SingleBook.jsx

--- client/src/pages/SingleBook.jsx
+++ client/src/pages/SingleBook.jsx
@@ -15,8 +15,7 @@ const SingleBook = () => {
     .then ((response) => {
       setBook(response.data)
       setLoading(false);
-       console.log(response.data);
-       console.log(error);
+       console.log("data",response.data);
     }).catch((error)=>{
       console.log(error);
       setLoading(false);
@@ -29,7 +28,7 @@ const SingleBook = () => {
       <h1 className="text-3xl my-4">Show Book</h1>
       {loading ? (<Spiner/>
       ):(
-        <div className="flex flex-col border-2
+      {book ? (   <div className="flex flex-col border-2
          border-sky-400 rounded-xl w-fit p-4">
           <div className="my-4">
             <span className='text-xl mr-4 text-gray-500'>Id</span>
@@ -60,7 +59,7 @@ const SingleBook = () => {
           </div>
        
 
-         </div>
+         </div>):(<h1>not book</h1>)}
       )}
     </div>
   )
